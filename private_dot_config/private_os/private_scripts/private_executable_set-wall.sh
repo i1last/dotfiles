28693261827da1a220900b1fdebbed78bc5a0d61
i1last <ac7you@gmail.com>
2023-10-17 00:05:23 +0300

    feat: update configs

--- private_dot_config/private_os/private_scripts/private_executable_set-wall.sh
+++ private_dot_config/private_os/private_scripts/private_executable_set-wall.sh
@@ -3,3 +3,4 @@
 wal -qni ${os}/walls/ --backend haishoku
 feh --bg-fill "$(< "$HOME/.cache/wal/wal")"
 wal-telegram --wal  # On Telegram Desktop: Settings -> Chat settings -> Choose from file -> "~/.cache/wal-telegram/*.tdesktop-theme" -> Keep changes
+pywal-discord  # for BetterDiscord
