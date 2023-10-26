f9baf09cbcc0543d18e9468cfaa278087f07ea9b
i1last <ac7you@gmail.com>
2023-10-26 14:49:59 +0300

    nvim: update hotkeys (add hotkeys for inkscape-figure)

--- private_dot_config/exact_nvim/exact_lua/exact_custom/settings/mappings.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_custom/settings/mappings.lua
@@ -3,7 +3,6 @@ local M = {}
 M.nvimtree = {
 	n = {
 		["<leader>cl"] = { "<cmd> NvimTreeCollapse <CR>", "Collaplse all in nvimtree" },
-		-- toggle: дублирую для приоритета над mg979/vim-visual-multi
 		["<C-n>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
 	},
 }
@@ -11,7 +10,26 @@ M.nvimtree = {
 M.texlab = {
 	n = {
 		["<leader>tt"] = { "<cmd> TexlabBuild <CR>", "Complile tex via TexLab" },
-    ["<leader>tz"] = { ":silent !zathura %:r.pdf &<CR>", "Open {this}.pdf in zathura" }
+		["<leader>tz"] = { [[:silent !zathura %:r.pdf &<CR>]], "Open {this}.pdf in zathura" },
+	},
+}
+
+M.inkscapefigures = {
+	n = {
+		["<leader>ef"] = {
+			[[<ESC>:silent execute "!inkscape-figures edit './figures/' > /dev/null 2>&1 &"<CR><CR>:redraw!<CR>]],
+			"Edit figure via inkscape-figures (for tex files)",
+		},
+		["<leader>iw"] = {
+			[[:silent execute "!inkscape-figures watch"<CR>]],
+      "Init inkscape-figures watcher (for tex files)"
+		},
+	},
+	i = {
+		["<leader>cf"] = {
+			[[<ESC>:silent execute ".!inkscape-figures create '" .. getline(".") .. "' './figures/'"<CR><CR>:w<CR>]],
+			"Create figure via inkscape-figures (for tex files)",
+		},
 	},
 }
 
