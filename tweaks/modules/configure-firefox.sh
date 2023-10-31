16f3ee17aee1e6f344e4b678094dde72a2620fb3
i1last <ac7you@gmail.com>
2023-10-31 16:10:37 +0300

    feat: remove tweak folder

--- tweaks/modules/configure-firefox.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Configure Chrome dir for userChrome settings
-firefoxDefaultReleaseIndex=$(ls $HOME/.mozilla/firefox/ | grep 'default-release' | sed 's/\..*//')
-mv ${CHEZMOI_SOURCE_DIR}/dot_mozilla/firefox/*.default-release/ ${CHEZMOI_SOURCE_DIR}/dot_mozilla/firefox/${firefoxDefaultReleaseIndex}.default-release/
-
-# Configure PyWalFox (pywal for Firefox)
-sudo pywalfox install
-firefox "https://addons.mozilla.org/en-US/firefox/addon/pywalfox/"
--- tweaks/modules/generate-pkgs-list.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash 
-
-mkdir -p $CHEZMOI_SOURCE_DIR/__tweaks/data/pkgs/
-
-pacman -Qe | cut -d " " -f 1 > $CHEZMOI_SOURCE_DIR/__tweaks/data/pkgs/pkgs-arch.txt
-pip freeze > $CHEZMOI_SOURCE_DIR/__tweaks/data/pkgs/pkgs-pip.txt
