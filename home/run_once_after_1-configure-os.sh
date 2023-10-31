723d892f41f5433a550258eb783d1413b02cc274
i1last <ac7you@gmail.com>
2023-10-31 23:03:45 +0300

    feat: update run_once scripts

--- home/run_once_after_1-configure-os.sh
+++ home/run_once_after_1-configure-os.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "\n\n=== === === === === === === === === ===\n\n"
+
 echo "Turn off beep..."
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 echo "...done"
@@ -8,3 +10,14 @@ echo "configure PyWalFox (pywal for Firefox)..."
 echo "Extensions on: https://addons.mozilla.org/en-US/firefox/addon/pywalfox"
 sudo pywalfox install
 echo "...done"
+
+echo "Setup http-server..."
+http-server ~/.config/chevron/dist/
+echo "...done"
+
+echo "Prepare neovim..."
+rm -rf $HOME/.local/share/nvim
+echo "...done"
+
+
+echo "\n\n=== === === === === === === === === ===\n\n"
