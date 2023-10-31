d0adf130c056162f8f4c0aa43140a325fc85c163
i1last <ac7you@gmail.com>
2023-10-31 16:11:16 +0300

    feat: add configure firefox pywal in run-1 script

--- home/run_once_1-configure-os.sh
+++ home/run_once_1-configure-os.sh
@@ -3,3 +3,8 @@
 echo "Turn off beep..."
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 echo "...done"
+
+echo "configure PyWalFox (pywal for Firefox)..."
+echo "Extensions on: https://addons.mozilla.org/en-US/firefox/addon/pywalfox"
+sudo pywalfox install
+echo "...done"
