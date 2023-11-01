3044a04c400e915e1e2d84e32b60763d51f8df93
i1last <ac7you@gmail.com>
2023-11-01 20:26:49 +0300

    feat: add alias for c++ compile

--- home/private_dot_zsh/aliases.zsh
+++ home/private_dot_zsh/aliases.zsh
@@ -67,3 +67,6 @@ alias t="tmux"
 alias what-is-git="echo -e '  ?   — untracked change(s);\n  +   — staged change(s);\n  !   — file(s) modified in the repo;\n  »   — renamed file(s);\n  —   — deleted file(s);\n  $   — stashed change(s);\n  #   — unmerged change(s);\n  •|  — behind of remote branch;\n  |•  — ahead of remote branch;\n  ~   — Branches have diverged;'"
 alias trt="trans -show-original n -show-original-phonetics n -show-translation-phonetics n -show-prompt-message n -show-dictionary n -show-alternatives n -show-languages n en:ru"
 alias cr="clear"
+
+# dev
+alias "cc++"="clang++ -std=c++20 -Wall"
