2d69ad187281fcf318f07349b4e8efd246a9bc27
i1last <ac7you@gmail.com>
2023-11-01 20:27:13 +0300

    fix: remove nvim eraser from run_0

--- home/run_once_after_1-configure-os.sh
+++ home/run_once_after_1-configure-os.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "\n\n=== === === === === === === === === ===\n\n"
+echo -e "\n\n=== === === === === === === === === ===\n\n"
 
 echo "Turn off beep..."
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
@@ -8,16 +8,11 @@ echo "...done"
 
 echo "configure PyWalFox (pywal for Firefox)..."
 echo "Extensions on: https://addons.mozilla.org/en-US/firefox/addon/pywalfox"
-sudo pywalfox install
+sudo pywalfox install &> /dev/null
 echo "...done"
 
-echo "Setup http-server..."
-http-server ~/.config/chevron/dist/
-echo "...done"
-
-echo "Prepare neovim..."
-rm -rf $HOME/.local/share/nvim
-echo "...done"
+echo "Prepare neovim if it need:"
+echo "rm -rf $HOME/.local/share/nvim"
 
 
-echo "\n\n=== === === === === === === === === ===\n\n"
+echo -e "\n\n=== === === === === === === === === ===\n\n"
